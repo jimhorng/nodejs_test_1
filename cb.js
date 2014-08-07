7d0ea2e7b77bb9d908a11d7173d20663c2fba384
jimhorng <jimhorng@qnap.com>
2014-08-07 10:20:07 +0800

    just reformat

--- cb.js
+++ cb.js
@@ -1,44 +1,44 @@
 angular.module('cbApp', [])
-  .controller('CBController', ['$scope', '$http', function($scope, $http) {
-    
-    $scope.cbs = {
-      '17043': {no: '17043'},
-      '16112': {no: '16112'},
-      '26105': {no: '26105'}
-    }
+    .controller('CBController', ['$scope', '$http', function($scope, $http) {
 
-    $scope.refreshCB = function() {
-      for( var cb_no in $scope.cbs ) {
-        var uri = '/cb/' + cb_no
-        var config = {
-          // data = Object;
-          responseType:"json"
+        $scope.cbs = {
+            '17043': {no: '17043'},
+            '16112': {no: '16112'},
+            '26105': {no: '26105'}
+        }
+
+        $scope.refreshCB = function() {
+            for( var cb_no in $scope.cbs ) {
+                var uri = '/cb/' + cb_no
+                var config = {
+                    // data = Object;
+                    responseType:"json"
+                };
+                $http.get(uri, config)
+                    .success((function(cb_no) {
+                        return function(response) {
+                            // var result = JSON.parse(result);
+                            console.log('cb_no: ' + cb_no);
+                            $scope.cbs[cb_no]['company_no'] = response.company_no;
+                            $scope.cbs[cb_no]['bond_short_name'] = response.bond_short_name;
+                            $scope.cbs[cb_no]['price'] = response.price;
+                            $scope.cbs[cb_no]['total_volume'] = response.total_volume;
+                            $scope.cbs[cb_no]['convert_price'] = response.convert_price;
+                            $scope.cbs[cb_no]['convert_share'] = Math.round(100000 / parseInt(response.convert_price));
+                        }
+                    })(cb_no))
+                    .error(function (data, status, headers, config) {
+                            if ( status == 404 ) {
+                                delete $scope.cbs[cb_no];
+                            }
+                        }
+                    );
+            }
         };
-        $http.get(uri, config)
-          .success((function(cb_no) {
-            return function(response) {
-              // var result = JSON.parse(result);
-              console.log('cb_no: ' + cb_no);
-              $scope.cbs[cb_no]['company_no'] = response.company_no;
-              $scope.cbs[cb_no]['bond_short_name'] = response.bond_short_name;
-              $scope.cbs[cb_no]['price'] = response.price;
-              $scope.cbs[cb_no]['total_volume'] = response.total_volume;
-              $scope.cbs[cb_no]['convert_price'] = response.convert_price;
-              $scope.cbs[cb_no]['convert_share'] = Math.round(100000 / parseInt(response.convert_price));
-            }
-          })(cb_no))
-          .error(function (data, status, headers, config) {
-              if ( status == 404 ) {
-                delete $scope.cbs[cb_no];
-              }
-            }
-          );
-      }
-    };
 
-    $scope.addCB = function() {
-      $scope.cbs[$scope.CBNo] = {no: $scope.CBNo};
-      $scope.CBNo = '';
-      $scope.refreshCB();
-    };
-  }]);
+        $scope.addCB = function() {
+            $scope.cbs[$scope.CBNo] = {no: $scope.CBNo};
+            $scope.CBNo = '';
+            $scope.refreshCB();
+        };
+    }]);
--- server.js
+++ server.js
@@ -6,19 +6,19 @@ var csvParser = require('csv-parse');
 
 var cb_list;
 var parser = csvParser({delimiter: ',', columns: true, trim: true}, function(err, data){
-  cb_list = data;
-  console.log("DEBUG: parse: " + JSON.stringify(cb_list[0]));
+    cb_list = data;
+    console.log("DEBUG: parse: " + JSON.stringify(cb_list[0]));
 });
 
 fs.createReadStream(__dirname+'/cb_list.csv').pipe(parser);
 
 var getCBInfo = function(cb_no) {
-  for ( var i = 0 ; i < cb_list.length ; i++ ) {
-    if( cb_no == cb_list[i].cb_no ) {
-      return cb_list[i];
+    for ( var i = 0 ; i < cb_list.length ; i++ ) {
+        if( cb_no == cb_list[i].cb_no ) {
+            return cb_list[i];
+        }
     }
-  }
-  return null;
+    return null;
 };
 
 var app = express();
@@ -26,34 +26,34 @@ var server = http.createServer(app);
 
 app.get('/cb/:cb_no', function(request, response){
 
-    var cb_no = request.params.cb_no;
+        var cb_no = request.params.cb_no;
 
-    var options = {
-      host: 'mis.tse.com.tw',
-      port: 80,
-      path: '/stock/api/getStockInfo.jsp?ex_ch=otc_' + cb_no + '.tw_20140806&json=1&delay=0',
-      method: 'GET'
-    };
+        var options = {
+            host: 'mis.tse.com.tw',
+            port: 80,
+            path: '/stock/api/getStockInfo.jsp?ex_ch=otc_' + cb_no + '.tw_20140806&json=1&delay=0',
+            method: 'GET'
+        };
 
-    http.request(options, function(res) {
-      res.on('data', function (data) {
-        var dataObj = JSON.parse(data);
-        var cb_info = getCBInfo(cb_no);
-        if (cb_info != null ) {
-          cb_info['price'] = dataObj.msgArray[0].z;
-          cb_info['total_volume'] = dataObj.msgArray[0].v;
-          response.set('Content-Type', 'application/json; charset=utf-8');
-          response.end(JSON.stringify(cb_info));
-        }
-        else {
-          cb_info = {};
-          response.send(404);
-        }
-      });
-    }).end();
+        http.request(options, function(res) {
+            res.on('data', function (data) {
+                var dataObj = JSON.parse(data);
+                var cb_info = getCBInfo(cb_no);
+                if (cb_info != null ) {
+                    cb_info['price'] = dataObj.msgArray[0].z;
+                    cb_info['total_volume'] = dataObj.msgArray[0].v;
+                    response.set('Content-Type', 'application/json; charset=utf-8');
+                    response.end(JSON.stringify(cb_info));
+                }
+                else {
+                    cb_info = {};
+                    response.send(404);
+                }
+            });
+        }).end();
 });
 app.use(express.static(__dirname));
 var port = Number(process.env.PORT || 8080);
 server.listen(port, function(){
-    console.log('starting web server on port:' + port + '...');
+        console.log('starting web server on port:' + port + '...');
 });
