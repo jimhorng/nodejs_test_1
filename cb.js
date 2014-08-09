688c33918b01953c98050cae41db051f7cf9f048
jimhorng <jimhorng@gmail.com>
2014-08-09 09:00:40 +0800

    test ng sort
    Conflicts:
    	cb.js

--- cb.js
+++ cb.js
@@ -1,7 +1,7 @@
-angular.module('cbApp', ['ui.bootstrap']);
+angular.module('cbApp', ['ui.bootstrap', 'ngTable']);
  
 
-var CBController = function($scope, $http, $modal) {
+var CBController = function($scope, $http, $modal, $filter, ngTableParams) {
 
     $scope.cbs = {
         '17043': {no: '17043'},
@@ -9,11 +9,17 @@ var CBController = function($scope, $http, $modal) {
         '26105': {no: '26105'}
     }
 
+    $scope.cbs_data = [
+        {cb_no: 2, price: 222},
+        {cb_no: 1, price: 111},
+        {cb_no: 3, price: 333}
+    ];
+
     $scope.refreshCB = function() {
+        $scope.cbs_data = [];
         for( var cb_no in $scope.cbs ) {
             var uri = '/cb/' + cb_no
             var config = {
-                // data = Object;
                 responseType:"json"
             };
             $http.get(uri, config)
@@ -21,6 +27,23 @@ var CBController = function($scope, $http, $modal) {
                     return function(response) {
                         console.log('cb_no: ' + cb_no);
                         $scope.cbs[cb_no] = response;
+                        $scope.cbs_data.push(response);
+
+                        $scope.tableParams = new ngTableParams({
+                            page: 1,            // show first page
+                            count: 10           // count per page
+                        }, {
+                            total: $scope.cbs_data.length, // length of data
+                            getData: function($defer, params) {
+                                // use build-in angular filter
+                                var orderedData = params.sorting() ?
+                                        $filter('orderBy')($scope.cbs_data, params.orderBy()) :
+                                        $scope.cbs_data;
+
+                                $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+                            }
+                        });
+
                     }
                 })(cb_no))
                 .error(function (data, status, headers, config) {
